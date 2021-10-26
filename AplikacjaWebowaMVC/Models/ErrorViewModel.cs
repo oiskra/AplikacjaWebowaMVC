192eb2549a23d8448421c733926c7fc5e5b5fcf7
ojask <ojask@DESKTOP-EKFVUHT>
2021-10-26 16:21:01 +0200

    Dodaj pliki projektów.

---
+++ AplikacjaWebowaMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AplikacjaWebowaMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
